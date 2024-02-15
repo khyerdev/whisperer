100376c2a72c041471ed4ad950b5389dcb0030ae
OSCH2008 <82794982+OSCH2008@users.noreply.github.com>
2024-02-14 19:04:45 -0900

    i got it working finally

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,33 @@ mod sem;
 
 use vector as vect;
 use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write, BufReader, BufWriter};
+use std::io::{Read, Write};
 use std::thread;
+
+const MAX_BUF_SIZE: usize = 1024usize;
 
 fn main() {
     let port = TcpListener::bind("0.0.0.0:9998").unwrap();
     
     thread::spawn(|| {
-        thread::sleep(std::time::Duration::from_secs(2));
         let mut connection = TcpStream::connect("192.168.40.126:9998").unwrap();
     
-        connection.write_all("test".as_bytes()).unwrap();
-        println!("sent");
+        connection.write_all("request".as_bytes()).unwrap();
 
-        let mut buf = String::new();
-        connection.read_to_string(&mut buf).unwrap();
-        println!("{buf}")
+        println!("{}", read_buffer(&mut connection))
     });
     
     for req in port.incoming() {
         let mut stream = req.unwrap();
+
+        println!("{}", read_buffer(&mut stream));
+
         stream.write_all("response".as_bytes()).unwrap();
-        println!("responded");
     }
+}
+
+fn read_buffer(stream: &mut TcpStream) -> String {
+    let mut buf = [0u8; MAX_BUF_SIZE];
+    stream.read(&mut buf).unwrap();
+    String::from_utf8_lossy(&buf).to_string()
 }
