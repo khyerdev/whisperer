0383d7874bbcc8dded7affad53d109bd9116d4eb
Khyernet <82794982+OSCH2008@users.noreply.github.com>
2024-03-10 13:41:36 -0800

    add symbolic link to allow running from the terminal

--- package/install.sh
+++ package/install.sh
@@ -4,6 +4,7 @@ mkdir $HOME/.local/share/applications
 sudo mkdir /usr/local/bin/whisperer
 sudo mv ./tcp.png /usr/local/bin/whisperer/tcp.png
 sudo mv ./whisperer /usr/local/bin/whisperer/whisperer
+sudo ln -s /usr/local/bin/whisperer/whisperer /usr/bin/whisperer
 mv ./whisperer.desktop $HOME/.local/share/applications/whisperer.desktop
 
 echo Successfully moved files
